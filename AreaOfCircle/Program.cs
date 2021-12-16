841bc4d997c159d55a7833147a1328f9814181ad
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-16 13:58:43 -0600

    circle

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AreaOfCircle_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AreaOfCircle();
+
+
+        }
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The radius of your circle is {Math.PI * Math.Pow(radius, 2)}");
+            
+
+        }
+    }
+
+
+
+
+
+}
